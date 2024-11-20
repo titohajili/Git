c559e9c8f5192848100b1d5d9976059159cf5a6e
titohajili <maqatoto963@gmail.com>
2024-11-20 23:54:40 +0400

    Keyboard accessibility for tab buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,3 +32,45 @@ addEventOnElements($searchTogglers, "click", function () {
     this.setAttribute("aria-expanded", isExpanded);
     $searchField.focus();
 });
+
+/**
+ * TAB NAVIGATION
+ */
+const $tabBtns = document.querySelectorAll("[data-tab-btn]");
+
+const $tabpanels = document.querySelectorAll("[data-tab-panel]");
+
+let [$lastActiveTabBtn] = $tabBtns;
+let [$lastActiveTabPanel] = $tabpanels;
+
+addEventOnElements($tabBtns, "click", function () {
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabPanel.setAttribute("hidden", "");
+
+    this.setAttribute("aria-selected", true);
+    const $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+
+    $lastActiveTabBtn = this;
+    $lastActiveTabPanel = $currentTabPanel;
+})
+
+
+/**
+ * Keyboard accessibility for tab buttons
+ */
+
+addEventOnElements($tabBtns, "keydown", function (e) {
+    const $nextElement = this.$nextElementSibling;
+    const $previousElement = this.$previousElementSibling;
+
+    if  (e.key === "ArrowRight" && $nextElement) {
+       this.setAttribute("tabindex", -1);
+       $nextElement.setAttribute("tabindex", 0);
+       $nextElement.focus();
+    } else if (e.key === "ArrowLeft" && $previousElement) {
+        this.setAttribute("tabindex", -1);
+        $previousElement.setAttribute("tabindex", 0);
+        $previousElement.focus();
+    }           
+});
